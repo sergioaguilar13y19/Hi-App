65b088da769e0bc9a84a2508b6e809d964ddc5e6
sergioaguilar13y19 <sergioaguilar13y19@gmail.com>
2023-10-25 12:42:18 -0600

    create app

---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export const SCREEN_WORDS = {
+  es: {
+    home: {
+      title: "Chats",
+    },
+    settings: {
+      title: "Ajustes",
+      darkMode: "Modo oscuro",
+      calls: "Llamada",
+      profile: "Perfil",
+    },
+    chat: {
+      header: {
+        icons: {
+          call: "Llamar",
+          video: "Video",
+          info: "Info",
+        },
+      },
+      input: {
+        placeholder: "Escribe un mensaje",
+      },
+    },
+  },
+};
